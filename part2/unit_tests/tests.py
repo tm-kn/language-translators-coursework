a3b7756b513cdea725db763bc51ac4e64e835f3b
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2017-04-28 18:04:12 +0100

    Add more unit tests and disable using null variables

--- part2/unit_tests/tests.py
+++ part2/unit_tests/tests.py
@@ -22,7 +22,6 @@ class TestTruthTable(TestParserMixin, unittest.TestCase):
             result = self.run_programme("{} {}".format('write', rule[0]))
 
             self.assertFinishedOKWith(result, rule[1])
-
     
     def test_or_truth_table(self):
         or_truth_table = [
@@ -42,7 +41,6 @@ class TestTruthTable(TestParserMixin, unittest.TestCase):
 
             self.assertFinishedOKWith(result, rule[1])
 
-
     def test_not_truth_table(self):
         not_truth_table = [
             ('!1', '0'),
@@ -55,6 +53,46 @@ class TestTruthTable(TestParserMixin, unittest.TestCase):
 
             self.assertFinishedOKWith(result, rule[1])
 
+
+class TestInterpretation(TestParserMixin, unittest.TestCase):
+    def test_valid_assignment_statements(self):
+        valid_assignments = [
+            ('k = 0; write k', '0'),
+            ('k = !1;write k', '0'),
+            ('k = 1; n = k;write k; write n', ['1', '1']), 
+            ('k=0;k=k; write k', '0'),
+            ('k=!!!0; n = k&0&?; write k; write n', ['1', '0'])
+        ]
+
+        for assignment in valid_assignments: 
+            result = self.run_programme(assignment[0])
+
+            self.assertFinishedOKWith(result, assignment[1])
+    
+    def test_valid_write_statements(self):
+        valid_write_statements = [
+            ('write 0&0', '0'),
+            ('k = 1; write k', '1'),
+            ('write ?', '?')
+
+        ]
+
+        for write in valid_write_statements:
+            result = self.run_programme(write[0])
+
+            self.assertFinishedOKWith(result, write[1])
+    
+    def test_using_null_variable_fails(self):
+        invalid_statements = [
+            'write k',
+            'k = k',
+            'n = 0 & k'
+        ]
+
+        for stm in invalid_statements:
+            result = self.run_programme(stm)
+
+            self.assertFinishedWithError(result)
 
 if __name__ == '__main__':
         unittest.main()
